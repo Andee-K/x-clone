4ee81979a576761a6277c8da782eac168b7996a6
Andrew <andrew3840@gmail.com>
2025-09-27 18:11:02 -0400

    updated final

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from "./HomePage.module.scss"
 import RightSidebar from '../RightSidebar'
 import Sidebar from '../Sidebar/Sidebar'
+import MainContent from '../MainContent/MainContent'
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
         <Sidebar avatarUrl='https://randomuser.me/api/portraits/men/1.jpg'></Sidebar>
+        <MainContent></MainContent>
         <RightSidebar></RightSidebar>
     </div>
   )
